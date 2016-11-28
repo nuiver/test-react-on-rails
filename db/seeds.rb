4ab7896f9f68acfc6db64407198c00e5dc47de3c
Mark Nuiver <mark@vrijblikken.nl>
2016-11-28 22:27:24 +0100

    Initial seeds products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+product1 = Product.create(name: 'Chainsaw', category: 'Tools', stocked: true , price: 240.75 )
+product2 = Product.create(name: 'Grinder', category: 'Tools', stocked: true , price: 120.35 )
+product3 = Product.create(name: 'Ruler', category: 'Tools', stocked: false , price: 18.50 )
+product4 = Product.create(name: 'Drill', category: 'Tools', stocked: true , price: 155.55 )
+product5 = Product.create(name: 'iPad', category: 'Electronics', stocked: false , price: 415.00 )
+product6 = Product.create(name: 'MacBook Pro', category: 'Electronics', stocked: true , price: 1995.25 )
+product7 = Product.create(name: 'iMac', category: 'Electronics', stocked: true , price: 2243.50 )
+product8 = Product.create(name: 'iPhone', category: 'Electronics', stocked: true , price: 899.99 )
