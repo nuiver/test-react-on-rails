536b64924e44c0c10ff40a1360df89875bc933fd
Mark Nuiver <mark@vrijblikken.nl>
2016-11-28 22:14:52 +0100

    Wiped out posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123161950) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.string   "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
