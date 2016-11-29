864d03df0cc6389b8d95586d72e0d9b16dea93eb
Mark Nuiver <mark@vrijblikken.nl>
2016-11-29 21:09:02 +0100

    Available product check

--- client/app/bundles/ProductTable/components/ProductRow.jsx
+++ client/app/bundles/ProductTable/components/ProductRow.jsx
@@ -6,13 +6,23 @@ class ProductRow extends React.Component {
     return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
   }
 
+  availableProductCheck(product) {
+    const redStyle = {
+      color: 'red',
+    }
+    if (product.stocked == true ) {
+      return (<span>{product.name}</span>)
+    }
+    return (<span style={redStyle}>{product.name}</span>)
+  }
+
   render() {
 
     let { product } = this.props
 
     return (
       <li>
-        <div>{ product.name }   { product.price }</div>
+        <div>{ this.availableProductCheck(product) }   { product.price }</div>
       </li>
     )
   }
